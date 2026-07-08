9a8cb69e018424b71430ae71ac6a9aaae58e7eab
zackpro94 <zekarais94@gmail.com>
2026-07-08 13:37:40 +0300

    Add media file serving for production to fix attachment display

--- lms_project/urls.py
+++ lms_project/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 # Customize admin site
 admin.site.site_header = 'Auction Ethiopia - LMS Admin'
@@ -14,6 +15,11 @@ urlpatterns = [
     path('', include('letters.urls')),
 ]
 
-# Serve media files in development
+# Serve media files in both development and production
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # In production, serve media files directly
+    urlpatterns += [
+        path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
