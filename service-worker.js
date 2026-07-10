4ba72c868ae5878aa99ac4bb4f31be3e8254b30c
zackpro94 <zekarais94@gmail.com>
2026-07-10 21:53:51 +0300

    Simplify service worker to remove missing icon references

--- service-worker.js
+++ service-worker.js
@@ -8,28 +8,13 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('push', (event) => {
-  const data = event.data.json();
+  const data = event.data ? event.data.json() : {};
   
   const options = {
     body: data.body || 'You have a new notification',
-    icon: '/static/icons/notification-icon.png',
-    badge: '/static/icons/badge-icon.png',
-    vibrate: [200, 100, 200],
     data: {
       url: data.url || '/',
-    },
-    actions: [
-      {
-        action: 'view',
-        title: 'View',
-        icon: '/static/icons/view-icon.png'
-      },
-      {
-        action: 'close',
-        title: 'Close',
-        icon: '/static/icons/close-icon.png'
-      }
-    ]
+    }
   };
   
   event.waitUntil(
@@ -40,9 +25,7 @@ self.addEventListener('push', (event) => {
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   
-  if (event.action === 'view') {
-    event.waitUntil(
-      clients.openWindow(event.notification.data.url || '/')
-    );
-  }
+  event.waitUntil(
+    clients.openWindow(event.notification.data.url || '/')
+  );
 });
